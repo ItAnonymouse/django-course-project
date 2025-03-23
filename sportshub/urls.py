bf545c5dbb35229d7fbb8a61ef107f961ef0c32e
ItAnonymouse <nwartellaii01@rio.edu>
2025-03-23 16:40:24 -0400

    Add files via upload

--- sportshub/urls.py
+++ sportshub/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from sportshub import views  # ✅ Make sure 'views.py' exists in sportshub
+from sportshub import views  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),  # ✅ This should work
+    path('', views.home, name='home'),  
     path('accounts/', include('accounts.urls')),
 ]
